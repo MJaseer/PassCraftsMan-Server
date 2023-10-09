82c7b17c8170df7c7b68a966bbcf28ebf0467f31
Muhammed Jaseer <mjaseer43@gmail.com>
2023-10-09 13:48:15 +0530

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ import cors from 'cors'
 const app = express()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://passcraftsman.netlify.app');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin',' https://passcraftsman.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next()
 })
 
