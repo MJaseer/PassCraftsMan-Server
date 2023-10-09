f3bd88309d24113671a113cf6cf4bd3ef3c03b10
Muhammed Jaseer <mjaseer43@gmail.com>
2023-10-09 13:40:52 +0530

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,19 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(
-    cors({
-        credentials: true,
-        origin: ['http://localhost:4200', 'https://passcrafter-x.netlify.app', "https://passcrafter-x.netlify.app", "https://passxgen.netlify.app"]
-    })
-);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://passcraftsman.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next()
+})
+
+app.use(cors({
+    credentials: true,
+    origin: 'https://passcraftsman.netlify.app',
+    methods: ["GET,HEAD,OPTIONS,POST,PUT,DELETE"]
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +28,7 @@ app.use(morgan("dev"))
 
 connectDataBase()
 
-app.use("/api",router)
+app.use("/api", router)
 
 const port = process.env.PORT
 
